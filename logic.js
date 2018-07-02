30e2803a62c8e7226d061b199ea3713024e652ba
Edward L. Davies <kungfoowiz@hotmail.com>
2018-07-02 06:09:14 +0100

    Part 1 of the javascript refactoring.

--- logic.js
+++ logic.js
@@ -31,64 +31,104 @@ $(function () {
     var audioCollapsible = new collapsible(".audio-control", "20px", "300px");
 
 
+
+
+
+    var disableActions = function () {
+        $(".action").attr("disabled", "disabled");
+    }
+
+
+
+    var performAttack = function (damage, title, targetHealth, targetHealthMax, targetProgressHealth, targetProgressText) {
+        alert(title + " attack dealing " + damage + " damage.");
+
+        targetHealth -= damage;
+
+        if (targetHealth <= 0) {
+            targetHealth = 0;
+        }
+
+        $(targetProgressHealth).attr("value", targetHealth);
+        $(targetProgressText).text(targetHealth + "/" + targetHealthMax);
+    }
+
+    var performRestore = function (restore, title, targetHealth, targetHealthMax, targetProgressHealth, targetProgressText) {
+        alert(title + " restored " + restore + " health.");
+
+        targetHealth += restore;
+
+        if (targetHealth >= targetHealthMax) {
+            targetHealth = targetHealthMax;
+        }
+
+        $(targetProgressHealth).attr("value", targetHealth);
+        $(targetProgressText).text(targetHealth + "/" + targetHealthMax);
+    }
+
+
 });
 
 
 
-var firstLife = true;
-var firstmaxHealth = 100;
-var firstHealth = 100;
+var youLife = true;
 
-var firstDamage = 10;
-var firstRestore = 5;
+var youHealth = 100;
+var youHealthMax = 100;
+var youDamage = 10;
+var youRestore = 5;
 
-var firstCounter = false;
-var firstEnergy = 100;
-var firstEnergyRestore = 20;
-
-var secondmaxHealth = 100;
-var secondHealth = 100;
-
-var secondDamage = 12;
-var secondRestore = 5;
-
-var secondCounter = false;
+var youCounter = false;
+var youEnergy = 100;
+var youEnergyRestore = 20;
 
 
-$(function(){
-    $("#firstProgress").attr("max", firstmaxHealth);
-    $("#firstProgress").attr("value", firstHealth);
-    $("#secondProgress").attr("max", secondmaxHealth);
-    $("#secondProgress").attr("value", secondHealth);
-    $("#fHealthDisplay").text(firstHealth + "/" + firstmaxHealth);
-    $("#sHealthDisplay").text(secondHealth + "/" + secondmaxHealth);
-    $("#fEnergyDisplay").text(firstEnergy);
+
+var enemyHealth = 100;
+var enemyHealthMax = 100;
+var enemyDamage = 12;
+var enemyRestore = 5;
+
+var enemyCounter = false;
+
+
+
+
+$(function () {
+
+    $(".you-health-progress").attr("value", youHealth);
+    $(".you-health-progress").attr("max", youHealthMax);
+    $(".you-health-text").text(youHealth + "/" + youHealthMax);
+    $(".you-energy-text").text(youHealth);
+
+    $(".enemy-health-progress").attr("value", enemyHealth);
+    $(".enemy-health-progress").attr("max", enemyHealthMax);
+    $(".enemy-health-text").text(enemyHealth + "/" + enemyHealthMax);
+
 });
+
+
+
+
 
 
 
 function youAttack(){
 
-    if (firstLife) {
+    if (youLife) {
 
-        $("button").attr("disabled", "disabled");
+        disableActions();
 
-        setTimeout(function(){
+        setTimeout(function () {
 
-            secondHealth -= firstDamage;
-
-            alert("You attack dealing " + firstDamage + " damage.");
+            performAttack(youDamage, "You", enemyHealth, enemyHealthMax, ".enemy-health-progress", ".enemy-health-text");
     
-            $("#secondProgress").attr("value", secondHealth);
-            $("#sHealthDisplay").text(secondHealth + "/" + secondmaxHealth);
-    
-
-            secondCounter = true;
-            firstCounter = false;
+            enemyCounter = true;
+            youCounter = false;
     
             winnerCheck();
     
-            if(secondHealth > 0){
+            if(enemyHealth > 0){
                 enemyMove();
             }
 
@@ -102,26 +142,21 @@ function youAttack(){
 
 function youRestore(){
 
-    if (firstLife) {
+    if (youLife) {
 
-        $("button").attr("disabled", "disabled");
-        setTimeout(function(){
-        firstHealth += firstRestore;
-            if(firstHealth >= firstmaxHealth){
-                firstHealth = firstmaxHealth;
-            }
-            $("#firstProgress").attr("value", firstHealth);
-            $("#fHealthDisplay").text(firstHealth + "/" + firstmaxHealth);
+        disableActions();
+
+        setTimeout(function () {
+
+            performRestore(youRestore, "You", youHealth, youHealthMax, ".enemy-health-progress", ".enemy-health-text");
     
             youEnergy();
     
-            alert("You restored " + firstRestore + " health and " + firstEnergyRestore + " energy.")
-    
-            secondCounter = false;
+            enemyCounter = false;
     
             winnerCheck();
     
-            if(secondHealth > 0){
+            if(enemyHealth > 0){
                 enemyMove();
             }
         }, 1100);
@@ -131,31 +166,55 @@ function youRestore(){
 }
 
 
+
+function youEnergy() {
+
+    winnerCheck();
+
+    if (youLife) {
+        youEnergy += youEnergyRestore;
+        $(".you-energy-text").text(youEnergy);
+
+        alert("You restored " + youEnergyRestore + " energy.");
+    }
+
+}
+
+
 function youCounter() {
 
-    if(secondHealth > 0){
+    if(enemyHealth > 0){
         enemyMove();
-        $("button").attr("disabled", "disabled");
+
+        disableActions();
     }
+
 
     setTimeout(function(){
 
-        $("button").attr("disabled", "disabled");
+        disableActions();
 
-        if (firstLife) {
+
+        if (youLife) {
+
             winnerCheck();
-            if (firstCounter) {
 
-                setTimeout(function(){
-                secondHealth -= (secondDamage * 2);
-                $("#secondProgress").attr("value", secondHealth);
-                $("#sHealthDisplay").text(secondHealth + "/" + secondmaxHealth);
-                alert("You countered dealing " + (secondDamage * 2) + " damage.");
-                $("button").removeAttr("disabled");
-                winnerCheck();
+            if (youCounter) {
+
+
+                setTimeout(function () {
+
+                    secondHealth -= (secondDamage * 2);
+                    $("#secondProgress").attr("value", secondHealth);
+                    $("#sHealthDisplay").text(secondHealth + "/" + secondmaxHealth);
+                    alert("You countered dealing " + (secondDamage * 2) + " damage.");
+                    $("button").removeAttr("disabled");
+                    winnerCheck();
+
                 }, 1100);
 
             }
+
 
             else {
 
@@ -169,24 +228,13 @@ function youCounter() {
 
             }
 
-            firstCounter = false;
-            secondCounter = false;
+            youCounter = false;
+            enemyCounter = false;
 
         }
 
     }, 1100);
 
-}
-
-
-function youEnergy() {
-
-    winnerCheck();
-
-    if (firstLife) {
-        firstEnergy += firstEnergyRestore;
-        $("#fEnergyDisplay").text(firstEnergy);
-    }
 
 }
 
@@ -221,11 +269,8 @@ function enemyAttack() {
 
     setTimeout(function () {
 
-        firstHealth -= secondDamage;
-        $("#firstProgress").attr("value", firstHealth);
-        $("#fHealthDisplay").text(firstHealth + "/" + firstmaxHealth);
-        
-        alert("Enemy attack dealing " + secondDamage + " damage.");
+        performAttack(enemyDamage, "Enemy", youHealth, youHealthMax, ".you-health-progress", ".you-health-text");
+
         firstCounter = true;
         secondCounter = false;
 
@@ -320,27 +365,38 @@ function enemyCounter(){
 
 
 
+
+
 function winnerCheck() {
 
-    if(firstHealth <= 0){
-        firstHealth = 0;
+
+    if(youHealth <= 0){
+
+        youHealth = 0;
         firstLife = false;
 
-        $("button").attr("disabled", "disabled");
+        disableActions();
+
         $("#firstProgress").attr("value", firstHealth);
         $("#fHealthDisplay").text(firstHealth + "/" + firstmaxHealth);
 
         alert("Enemy win");
+
     }
 
+
     if(secondHealth <= 0){
+
         secondHealth = 0;
 
-        $("button").attr("disabled", "disabled");
+        disableActions();
+
         $("#secondProgress").attr("value", secondHealth);
         $("#sHealthDisplay").text(secondHealth + "/" + secondmaxHealth);
 
         alert("You win");
+
     }
+
 
 }
